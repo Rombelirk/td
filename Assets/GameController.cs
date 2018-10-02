a2e1b0175ec77698f55b176661c6acb8b938be66
Roman <Rombelirk@gmail.com>
2018-10-03 00:05:39 +0300

    initial commit

---
+++ Assets/GameController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+
+	public void StartNewGame() {
+        Debug.Log("ok");
+        SceneManager.LoadScene("Scenes/Level1");
+      
+    }
+
+    void Update() {
+      
+    }
+
+  
+}
