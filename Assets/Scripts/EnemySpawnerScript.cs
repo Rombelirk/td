41a963af7b23b7a09f8f93430cefe2ee1d44f594
Roman <Rombelirk@gmail.com>
2018-10-07 12:23:15 +0300

    army of soldiers

---
+++ Assets/Scripts/EnemySpawnerScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnerScript : MonoBehaviour {
+
+    public float timeToSpawn = 4;
+    int spawnCount = 0;
+    public GameObject enemyPref, waypointParent;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (timeToSpawn <= 0) {
+            StartCoroutine(SpawnEnemy(spawnCount + 1));
+            timeToSpawn = 4;
+        }
+
+        timeToSpawn -= Time.deltaTime;
+	}
+
+    IEnumerator SpawnEnemy(int enemyCount) {
+
+        spawnCount++;
+
+        for (int i = 0; i < enemyCount; i++) {
+            GameObject tmpEnemy = Instantiate(enemyPref);
+            tmpEnemy.transform.SetParent(gameObject.transform, false);
+            tmpEnemy.GetComponent<EnemyScript>().waypointsParent = waypointParent;
+            yield return new WaitForSeconds(0.2f);
+
+        }
+
+
+    }
+}
