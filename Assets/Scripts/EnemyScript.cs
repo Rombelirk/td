514b3c841d9e2f87b676b9a4945ce40ce26b0dac
Roman <Rombelirk@gmail.com>
2018-10-07 11:06:10 +0300

    soldier running щт еру кщфв

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour {
 
+    public GameObject child;
+
     List<GameObject> wayPoints = new List<GameObject>();
     int wayIndex = 0;
     int speed = 1;
+
 	// Use this for initialization
 	void Start () {
         wayPoints = GameObject.Find("Main Camera").GetComponent<GameControllerScript>().wayPoints;
@@ -19,12 +22,23 @@ public class EnemyScript : MonoBehaviour {
 
 
     void Move() {
+      
         Vector3 dir = wayPoints[wayIndex].transform.position - transform.position;
         transform.Translate(dir.normalized * Time.deltaTime * speed);
 
-        if(Vector3.Distance(transform.position, wayPoints[wayIndex].transform.position) < 0.3f) {
+        var direct = wayPoints[wayIndex].transform.position - child.transform.position;
+        var angle = Mathf.Atan2(direct.y, direct.x) * Mathf.Rad2Deg + -90;
+        child.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+
+        if (Vector3.Distance(transform.position, wayPoints[wayIndex].transform.position) < 0.1f) {
+
+         
+
             if (wayIndex < wayPoints.Count - 1) {
                 wayIndex++;
+
+
             } else {
                 Destroy(gameObject);
             }
