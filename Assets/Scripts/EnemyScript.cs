72a2967f2c4f19db4d77913ca7ed1d1c052bc25c
Roman <Rombelirk@gmail.com>
2018-10-07 12:22:54 +0300

    army of soldiers

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour {
 
+    public GameObject waypointsParent;
     public GameObject child;
 
     List<GameObject> wayPoints = new List<GameObject>();
